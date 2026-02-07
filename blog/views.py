14e318c74c3ea6661c6f4849c1c99409ede1a125
siddharthjonior7-ux <siddharthjonior7@gmail.com>
2026-02-07 19:45:06 +0530

    Restore missing post_list and post_detail views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .models import Post
 from .forms import PostForm
+
+
+def post_list(request):
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()
+    ).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 @login_required
 def post_new(request):
